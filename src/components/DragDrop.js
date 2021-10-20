a1c09324766e0cb3409673a2e9e6b047db7cdd5e
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-10-20 11:04:16 +0530

    New

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -12,11 +12,6 @@ const PictureList = [
   {
     id: 2,
     url:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQExheo0sff_yQ/profile-displayphoto-shrink_200_200/0/1590072898568?e=1630540800&v=beta&t=_W-gWZewSBYQ-UAjpGvR8h_8Vvo202IHQQissbK2aKc",
-  },
-  {
-    id: 3,
-    url:
       "https://yt3.ggpht.com/pe57RF1GZibOWeZ9GwRWbjnLDCK2EEAeQ3u4iMAFNeaz-PN9uSsg1p2p32TZUedNnrUhKfoOuMM=s900-c-k-c0x00ffffff-no-rj",
   },
 ];
@@ -40,12 +35,12 @@ function DragDrop() {
     <>
       <div className="Pictures">
         {PictureList.map((picture) => {
-          return <Picture url={picture.url} id={picture.id} />;
+          return <Picture url={picture.url} id={picture.id}  />;
         })}
       </div>
       <div className="Board" ref={drop}>
         {board.map((picture) => {
-          return <Picture url={picture.url} id={picture.id} />;
+          return <Picture url={picture.url} id={picture.id} alt="Image" />;
         })}
       </div>
     </>
